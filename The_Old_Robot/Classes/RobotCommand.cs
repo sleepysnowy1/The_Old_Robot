738b358240b2e2d589e40325c1bcc720bc14f1b7
sleepysnowy1 <nelsonmason839@gmail.com>
2024-03-21 18:56:41 -0500

    Add project files.

---
+++ The_Old_Robot/Classes/RobotCommand.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Old_Robot.Classes
+{
+    internal abstract class RobotCommand
+    {
+        public abstract void Run(Robot robot);
+    }
+
+    internal class OnCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+
+            if (robot.IsPowered == false)
+            {
+                robot.IsPowered = true; 
+            }
+        }
+    }
+
+    internal class OffCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if(robot.IsPowered == true)
+            {
+                robot.IsPowered = false;
+            }
+        }
+    }
+
+    internal class NorthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered == true)
+            {
+                robot.Y += 1; 
+            }    
+        }
+    }
+
+    internal class SouthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered == true)
+            {
+                robot.Y += -1; 
+            } 
+        }
+    }
+
+    internal class WestCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered == true)    
+            {
+                robot.X += -1;
+            }
+        }
+    }
+
+    internal class EastCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered == true)
+            {
+                robot.X += 1; 
+            }
+        }
+    }
+}
---
+++ The_Old_Robot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Old_Robot.Classes;
+
+namespace The_Old_Robot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot MyRobot = new Robot();
+            
+            EastCommand eastCommand = new EastCommand();
+            WestCommand westCommand = new WestCommand();
+            NorthCommand northCommand = new NorthCommand();
+            SouthCommand southCommand = new SouthCommand();
+            OnCommand onCommand = new OnCommand();
+
+            MyRobot.Commands[0] = onCommand;
+            MyRobot.Commands[1] = westCommand;
+            MyRobot.Commands[2] = northCommand;
+
+
+            MyRobot.Run();
+
+            Console.ReadLine(); 
+        }
+    }
+}
